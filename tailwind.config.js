c5fec312c1581f2affa9ebba24e9065e9d27a37b
kangozan <ahmadfauzannn2002@gmail.com>
2024-09-03 12:40:40 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        navi: "#16325B",
+        teal: "#227B94",
+        blue: "78B7D0",
+        yellow: "#FFDC7F",
+      },
+    },
+  },
+  plugins: [],
+};
